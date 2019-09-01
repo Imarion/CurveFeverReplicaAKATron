de1f2ceff7f9ac5e7d500aa7b0818fa7f97b6e1e
Manu <black_falcon00@hotmail.com>
2019-09-01 19:25:13 +0200

    Done

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    private bool hasEnded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EndGame()
+    {
+        if (hasEnded)
+            return;
+
+        hasEnded = true;
+        StartCoroutine(PlayEndGameAnimation());
+    }
+
+    private IEnumerator PlayEndGameAnimation()
+    {
+        Debug.Log("Game over.");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    public float speed = 3f;
+    public float rotationSpeed = 200f;
+    public string inputAxis = "Horizontal";
+
+    float horizontal = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw(inputAxis);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector2.up * speed * Time.fixedDeltaTime, Space.Self); // as it is: take rotation into account
+        transform.Rotate(Vector3.forward * -horizontal * rotationSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "killsPlayer")
+        {
+            speed = 0f;
+            rotationSpeed = 0f;
+            GameObject.FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
